cd3dc8dcb404824d8f99abb4277a3423deac1796
alirezajawadi <alirezajavadigit@gmail.com>
2024-04-25 22:46:31 +0330

    Create CRM class with singleton pattern
    Initial commit for CRM class implementing singleton pattern.

---
+++ src/App/CRM.php
@@ -0,0 +1,16 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Created By Alireza Javadi
+|--------------------------------------------------------------------------
+*/
+
+namespace VibeTiger\App;
+
+use VibeTiger\System\Traits\MethodCaller;
+use VibeTiger\System\Traits\Singleton;
+
+class CRM
+{
+    use MethodCaller, Singleton;
+}
