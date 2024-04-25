e54ceff4259a2ff31d2a7382ff2a2dbbdd8edd36
alirezajawadi <alirezajavadigit@gmail.com>
2024-04-26 00:36:01 +0330

    Update CRM class properties
    Updated CRM class to include properties for CRM system URL, username, access key, and kernel instance.

--- src/App/CRM.php
+++ src/App/CRM.php
@@ -13,4 +13,32 @@ use VibeTiger\System\Traits\Singleton;
 class CRM
 {
     use MethodCaller, Singleton;
+
+    /**
+     * The URL for accessing the CRM system.
+     *
+     * @var string|null The URL for accessing the CRM system.
+     */
+    private static $url;
+
+    /**
+     * The username for authentication.
+     *
+     * @var string|null The username for authentication.
+     */
+    private static $username;
+
+    /**
+     * The access key for authentication.
+     *
+     * @var string|null The access key for authentication.
+     */
+    private static $accessKey;
+
+    /**
+     * The kernel instance for interacting with the CRM system.
+     *
+     * @var Kernel|null The kernel instance for interacting with the CRM system.
+     */
+    private static $kernel;
 }
