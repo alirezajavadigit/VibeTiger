890385025fe0679a3a6e6ea994c59d42d3a83259
alirezajawadi <alirezajavadigit@gmail.com>
2024-04-25 22:47:23 +0330

    Create MethodCaller trait
    Initial commit for MethodCaller trait to enable dynamic method calling.

---
+++ src/System/Traits/MethodCaller.php
@@ -0,0 +1,51 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Created By Alireza Javadi
+|--------------------------------------------------------------------------
+*/
+
+namespace VibeTiger\System\Traits;
+
+trait MethodCaller
+{
+    /**
+     * Magic method to call instance methods dynamically.
+     *
+     * @param string $method The method name to call.
+     * @param array $args An array of arguments to pass to the method.
+     * @return mixed The result of the method call.
+     */
+    public function __call($method, $args)
+    {
+        // Call the methodCaller method of the current instance with provided arguments.
+        return self::$instance->methodCaller(self::$instance, $method, $args);
+    }
+
+    /**
+     * Magic method to call static methods dynamically.
+     *
+     * @param string $method The method name to call.
+     * @param array $args An array of arguments to pass to the method.
+     * @return mixed The result of the method call.
+     */
+    public static function __callStatic($method, $args)
+    {
+        // Call the methodCaller method of the current instance with provided arguments.
+        return self::$instance->methodCaller(self::$instance, $method, $args);
+    }
+
+    /**
+     * Calls a method dynamically.
+     *
+     * @param object $object The object to call the method on.
+     * @param string $method The method name to call.
+     * @param array $args An array of arguments to pass to the method.
+     * @return mixed The result of the method call.
+     */
+    private function methodCaller($object, $method, $args)
+    {
+        // Call the method on the object with provided arguments.
+        return call_user_func_array(array($object, $method), $args);
+    }
+}
